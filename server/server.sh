d3263c58b05e742eab89fd52984a44a5c6c4e8e8
late <coolboy1353@163.com>
2014-09-06 10:27:54 +0800

    整理server代码，添加others文件夹，存放开发中的产物

--- server/server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ -o server loadbalance.cpp server.cpp online_client.cpp msghandler.cpp message.cpp client_info.cpp cJSON.cpp dbconnect.cpp tools.cpp -I/usr/include/mysql/mysql -L/usr/lib/mysql -lmysqlclient
-
--- unit_test/test_message.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ test_message.cpp message.cpp cJSON.c dbconnect.cpp
